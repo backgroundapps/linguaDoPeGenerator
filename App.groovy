a12db9ba58682b4639c1597841d51d0dc902e94b
Jonatas Emidio <jonatasemidio@gmail.com>
2015-07-03 14:33:51 -0300

    Update App.groovy
    Inclusão da metaClass para a geração de validação

--- App.groovy
+++ App.groovy
@@ -1,2 +1,5 @@
 //Apenas um exemplo para ter uma ideia de como começar o App
+
+String.metaClass.getFirstIsVowel{-> return delegate[0] ==~ /[aeiou]/}
+
 nw = 'jonatas'.collect{ if(it.firstIsVowel){return it+"#"}else{return it}}.join().split('#').collect{it+'p'+it[-1]}
